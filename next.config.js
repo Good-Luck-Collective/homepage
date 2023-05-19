607c37bd7d4a69723a98e32c48c3430735e81fc9
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-19 23:42:31 +0200

    fix: remove base path

--- next.config.js
+++ next.config.js
@@ -7,8 +7,6 @@ const withPWA = require("next-pwa")({
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   output: "export",
   images: {
     loader: "akamai",
